dfde4eb63624d70f208105470eb91ac61956fcc8
Dawntraoz <albasilventefu@gmail.com>
2022-10-02 17:04:08 +0200

    feat: add the layout showing the active link

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="w-full h-24 bg-[#f7f6fd]">
+    <div class="container h-full mx-auto flex items-center justify-between">
+      <NuxtLink to="/">
+        <h1 class="text-[#50b0ae] text-3xl font-bold">Storyblok Nuxt</h1>
+      </NuxtLink>
+      <nav>
+        <ul class="flex space-x-8 text-lg font-bold">
+          <li v-for="link in ['blog', 'about']">
+            <NuxtLink :to="`/${link}`" class="capitalize hover:text-[#50b0ae]">{{ link }}</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+nav a.router-link-active {
+  @apply underline underline-offset-4 decoration-4 decoration-[#50b0ae];
+}
+</style>
