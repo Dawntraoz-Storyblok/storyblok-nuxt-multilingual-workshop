190266cb9b779f115103858b5f61d694cd6476ee
Dawntraoz <albasilventefu@gmail.com>
2022-10-03 19:24:40 +0200

    feat: set up locales variables

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,12 +1,18 @@
 <script setup>
 const { slug } = useRoute().params;
 
+const locales = useState('locales')
+const locale = useState('locale')
+const isLocale = locales.value.some(loc => loc === slug[0])
+if (isLocale) slug.shift()
+
 const resolveRelations = ['popular-articles.articles']
 
 const story = await useAsyncStoryblok(
-  slug ? slug.join('/') : 'home',
+  slug && slug.length > 0 ? slug.join('/') : 'home',
   {
     version: 'draft',
+    language: locale.value,
     resolve_relations: resolveRelations, // Resolved for the API request
   },
   {
