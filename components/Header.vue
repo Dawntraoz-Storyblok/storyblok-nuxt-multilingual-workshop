8fded1104dafa7a797cf9f0c4ed9a4f390144613
Dawntraoz <albasilventefu@gmail.com>
2022-10-03 19:30:47 +0200

    feat: add basic language switcher

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,25 @@
 <template>
   <header class="w-full h-24 bg-[#f7f6fd]">
     <div class="container h-full mx-auto flex items-center justify-between">
-      <NuxtLink to="/">
+      <NuxtLink :to="localeUrl() || '/'">
         <h1 class="text-[#50b0ae] text-3xl font-bold">Storyblok Nuxt</h1>
       </NuxtLink>
       <nav v-if="headerMenu">
-        <ul class="flex space-x-8 text-lg font-bold">
+        <ul class="flex items-center space-x-8 text-lg font-bold">
           <li v-for="blok in headerMenu" :key="blok._uid">
-            <NuxtLink :to="blok.link.cached_url" class="hover:text-[#50b0ae]">
+            <NuxtLink :to="`${localeUrl()}/${blok.link.cached_url}`" class="hover:text-[#50b0ae]">
               {{ blok.link.story.name }}
             </NuxtLink>
+          </li>
+          <li class="flex">
+            <span
+              v-for="loc in locales"
+              :class="{ 'bg-black text-white': (loc === locale) }"
+              class="block uppercase px-4 py-1 md:p-2 rounded-lg lg:px-4 cursor-pointer"
+              @click="changeLocale(loc)"
+            >
+              {{ loc }}
+            </span>
           </li>
         </ul>
       </nav>
@@ -17,7 +27,17 @@
   </header>
 </template>
 
+
 <script setup>
+const router = useRouter()
+const locales = useState('locales')
+const locale = useState('locale')
+const localeUrl = () => locale.value !== 'en' ? `/${locale.value}` : ''
+const changeLocale = (loc) => {
+  locale.value = loc
+  router.push({ path: localeUrl() || '/' })
+}
+
 const storyblokApi = useStoryblokApi()
 const { data } = await storyblokApi.get('cdn/stories/config', {
   version: 'draft',
